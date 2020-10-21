445907c8f2842105006e3b4a253e96d3945b564a
brendino500 <brenda.ty@live.com>
2020-10-21 17:46:23 +0100

    cards styled

--- frontend/src/plants/PlantCard.tsx
+++ frontend/src/plants/PlantCard.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
-import { PlantType } from './PlantType';
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Grid,
+} from "@material-ui/core";
+import { PlantType } from "./PlantType";
 
 const useStyles = makeStyles({
   root: {
-    margin: 20
+    margin: 20,
   },
   media: {
     height: 250,
+    width: 250,
+  },
+  button: {
+    color: "#f2e5c6",
+    fontFamily: "Rubik",
+    fontWeight: "bold",
+  },
+  text: {
+    color: "#f2e5c6",
+    fontFamily: "Rubik",
+    fontWeight: "bold",
+    fontSize: 13,
+  },
+  price: {
+    color: "#f2e5c6",
+    fontFamily: "Old Standard TT",
+  },
+  paper: {
+    backgroundColor: "#2a5034",
   },
 });
 
@@ -19,26 +47,28 @@ export default function PlantCard(props: PlantType) {
   return (
     <Card className={classes.root}>
       <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={props.image}
-        />
-        <CardContent>
-          <Typography gutterBottom>
+        <CardMedia className={classes.media} image={props.image} />
+        <CardContent className={classes.paper}>
+          <Typography className={classes.text} gutterBottom>
             {props.name}
           </Typography>
-          <Typography>
-            £{props.price}.00
-          </Typography>
+          <Typography className={classes.price}>£{props.price}.00</Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          View 
-        </Button>
-        <Button size="small" color="primary">
-          Add to Cart
-        </Button>
+      <CardActions className={classes.paper}>
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+        >
+          <Button className={classes.button} size="small">
+            View
+          </Button>
+          <Button className={classes.button} size="small">
+            Add to Cart
+          </Button>
+        </Grid>
       </CardActions>
     </Card>
   );
--- frontend/src/plants/PlantIndex.tsx
+++ frontend/src/plants/PlantIndex.tsx
@@ -1,43 +1,57 @@
-import React from 'react'
-import { getAllPlants } from '../lib/api'
-import PlantCard from './PlantCard'
+import React from "react";
+import { getAllPlants } from "../lib/api";
+import { PlantType } from "./PlantType";
+import PlantCard from "./PlantCard";
 
-import { makeStyles } from '@material-ui/core/styles';
-import { PlantType } from './PlantType';
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Typography, Grid } from "@material-ui/core";
 
 export default function PlantIndex() {
-
   const useStyles = makeStyles({
-  root: {
-    padding: 10
-  }
+    root: {
+      padding: 10,
+      backgroundColor: "#25462e",
+    },
+    title: {
+      fontFamily: "Old Standard TT",
+      fontSize: 35,
+      letterSpacing: 3,
+      textTransform: "uppercase",
+      textAlign: "center",
+      color: "#fff9ed",
+    },
   });
 
-  const [data, setData] = React.useState([])
+  const [data, setData] = React.useState([]);
   const classes = useStyles();
 
   const getData = async () => {
-    const res = await getAllPlants()
-
-    setData(res.data)
-  }
+    const res = await getAllPlants();
+    setData(res.data);
+  };
 
   React.useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
-  if (!data) return null
+  if (!data) return null;
   return (
-
-    <>
-      <div>
-      {data.map((plant : PlantType) => (
-        <PlantCard key={plant._id} {...plant} />
-      ))}
-      </div>
-    </>
-    // <PlantCard />
-  )
+    <div className={classes.root}>
+      <Typography className={classes.title}>
+        <b>House Plants</b>
+      </Typography>
+      <Container>
+        <Grid
+          container
+          direction="row"
+          justify="space-evenly"
+          alignItems="center"
+        >
+          {data.map((plant: PlantType) => (
+            <PlantCard key={plant._id} {...plant} />
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
 }
-
-
