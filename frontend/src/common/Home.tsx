d19dfebf5e6bbd98902b6679f18ff9396a1ab25a
brendino500 <brenda.ty@live.com>
2020-10-22 16:27:53 +0100

    comp restart

--- frontend/src/common/Home.tsx
+++ frontend/src/common/Home.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import image from "../assets/zzplant.jpg";
 
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, Typography } from "@material-ui/core";
+import { Container, Typography, Button } from "@material-ui/core";
 
 const useStyles = makeStyles({
   "@global": {
@@ -23,6 +24,14 @@ const useStyles = makeStyles({
     marginTop: "10%",
     color: "#1A3400",
   },
+  button: {
+    fontFamily: "Open Sans",
+    marginTop: "2%",
+    color: "#1A3400",
+  },
+  link: {
+    textDecoration: "none",
+  },
 });
 
 export default function Home() {
@@ -33,6 +42,11 @@ export default function Home() {
       <Typography className={classes.title}>
         Jungle {<br />}København
       </Typography>
+      <Link to={`/plants`} className={classes.link}>
+        <Button className={classes.button} variant="outlined">
+          Show me the plants
+        </Button>
+      </Link>
     </Container>
   );
 }
