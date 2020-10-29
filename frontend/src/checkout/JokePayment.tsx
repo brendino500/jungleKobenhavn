79b15028b64d0bf910d0807589f15cb23a3ba138
brendino500 <brenda.ty@live.com>
2020-10-29 14:39:04 +0000

    conact page final

--- frontend/src/checkout/JokePayment.tsx
+++ frontend/src/checkout/JokePayment.tsx
@@ -10,6 +10,8 @@ import {
   Grid,
 } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import LinkIcon from "@material-ui/icons/Link";
 
 const useStyles = makeStyles({
   root: {
@@ -47,15 +49,24 @@ const useStyles = makeStyles({
     color: "#848380",
     fontSize: 14,
   },
-  psycheSection: {
-    // // display: "flex",
-    // // flexDirection: "column",
-    // flexGrow: 1,
+  iconText: {
+    fontFamily: "Open Sans",
+    color: "#365902",
+    fontSize: 14,
+    marginLeft: "15%",
   },
   contactSection: {
     display: "flex",
     flexDirection: "row",
     maxWidth: 400,
+    marginTop: "7%",
+  },
+  icons: {
+    color: "#365902",
+    fontSize: 27,
+  },
+  contactList: {
+    marginTop: "3%",
   },
 });
 
@@ -70,7 +81,7 @@ export default function JokePayment() {
         justify="space-between"
         alignItems="flex-start"
       >
-        <div className={classes.psycheSection}>
+        <div>
           <Container>
             <Typography className={classes.title}>Psyche!</Typography>
           </Container>
@@ -136,18 +147,57 @@ export default function JokePayment() {
           </Container>
         </div>
         <Container className={classes.contactSection}>
-          <Typography className={classes.contactText}>
-            Hi, I'm Brenda and I made this site all by myself after 3 months of
-            coding.
-            {<br />}
-            {<br />}
-            I'm currently looking for a front end developer role. If you would
-            like to see more of my projects or even give me a stalk, please
-            check out these links below:
-          </Typography>
-          <List>
-            <ListItem></ListItem>
-          </List>
+          <Grid
+            container
+            direction="column"
+            justify="flex-start"
+            alignItems="center"
+          >
+            <Typography className={classes.contactText}>
+              Hi, I'm Brenda and I made this site all by myself after 3 months
+              of coding.
+              {<br />}
+              {<br />}
+              I'm currently looking for a front end developer role. If you would
+              like to see more of my projects or even give me a stalk, please
+              check out these links below:
+            </Typography>
+            <List className={classes.contactList}>
+              <a
+                href="https://github.com/brendino500"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classes.link}
+              >
+                <ListItem>
+                  <GitHubIcon className={classes.icons} />
+                  <Typography className={classes.iconText}>GitHub</Typography>
+                </ListItem>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/brendaty/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classes.link}
+              >
+                <ListItem>
+                  <LinkedInIcon className={classes.icons} />
+                  <Typography className={classes.iconText}>LinkedIn</Typography>
+                </ListItem>
+              </a>
+              <a
+                href="https://brendaty.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classes.link}
+              >
+                <ListItem>
+                  <LinkIcon className={classes.icons} />
+                  <Typography className={classes.iconText}>Website</Typography>
+                </ListItem>
+              </a>
+            </List>
+          </Grid>
         </Container>
       </Grid>
     </Container>
