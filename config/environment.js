3459f4d2e33f423580e4121d0eb00e842a6aae2b
brendino500 <brenda.ty@live.com>
2020-11-11 17:04:55 +0000

    test deploy on netlify

--- config/environment.js
+++ config/environment.js
@@ -1,9 +1,9 @@
-const PORT = process.env.PORT || 2000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/jungle-kobenhavn'
-const secret = process.env.SECRET || 'randomsecretstring'
+const PORT = process.env.PORT || 2000;
+const dbURI = process.env.MONGODB_URI || "mongodb://localhost/jungle-kobenhavn";
+const secret = process.env.SECRET || "randomsecretstring";
 
 module.exports = {
   PORT,
   dbURI,
-  secret
-}
+  secret,
+};
